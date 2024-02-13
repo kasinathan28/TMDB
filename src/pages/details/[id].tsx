eb5883b5b168c3b6aaf0fc3e958847e217a4408a
Kasinathan <way2kasinathb35@gmail.com>
2024-02-13 22:48:00 +0530

    solved the issue on the details page in pages

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -1,38 +1,40 @@
-// pages/details/[id].tsx
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { fetchMovieDetails } from "@/modules/details/services/movieDetails";
 import styles from "@/themes/detailsPage.module.css";
 import Navbar from "@/modules/dashboard/components/navbar";
+
+
 const DetailsPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [movieDetails, setMovieDetails] = useState(null);
+  const [movieDetails, setMovieDetails] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const getMovieDetails = async () => {
+      if (!id) return; // Check if id is undefined
       try {
         setLoading(true);
-        const details = await fetchMovieDetails(id);
+        const details = await fetchMovieDetails(id as string); // Type assertion
+        console.log(details.backdrop_path);
+        
         setMovieDetails(details);
       } catch (error) {
-        setError(error);
+        // setError(error);
         console.error("Error fetching movie details:", error);
       } finally {
         setLoading(false);
       }
     };
 
-    if (id) {
-      getMovieDetails();
-    }
+    getMovieDetails();
   }, [id]);
 
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
       <div className={styles.container}>
         {loading && <p className={styles.loading}>Loading...</p>}
         {error && <p className={styles.error}>{error}</p>}
@@ -44,20 +46,18 @@ const DetailsPage = () => {
             />
             <div className={styles.overlay}>
               <div className={styles.content}>
-
-              <div className={styles.poster}>
-                <img
-                  src={`https://image.tmdb.org/t/p/original/${movieDetails.poster_path}`}
-                  alt={movieDetails.poster_path}
-                />
-              </div>
-              <div>
-
-              <h2>{movieDetails.title}</h2>
-              <p>{movieDetails.overview}</p>
-              <p>Popularity: {movieDetails.popularity}</p>
-              <p>Release Date: {movieDetails.release_date}</p>
-              </div>
+                <div className={styles.poster}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${movieDetails.poster_path}`}
+                    alt={movieDetails.poster_path}
+                  />
+                </div>
+                <div>
+                  <h2>{movieDetails.title}</h2>
+                  <p>{movieDetails.overview}</p>
+                  <p>Popularity: {movieDetails.popularity}</p>
+                  <p>Release Date: {movieDetails.release_date}</p>
+                </div>
               </div>
             </div>
           </div>
